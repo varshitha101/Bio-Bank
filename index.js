c533ebe178183c7b18bda08ff8cc0aac27a3b52f
varshitha101 <95077762+varshitha101@users.noreply.github.com>
2024-11-07 13:36:15 +0530

    Slot boxes Updated in index.js

--- index.js
+++ index.js
@@ -1,20 +1,147 @@
 function test1() {
   let seats1 = document.querySelector(".blood-box");
-  let seat1Array = []; 
+  let seat1Array = [];
+  let k=0;
+
   for (var i = 0; i < 54; i++) {
-      let booked = "";
-      if (i >= 0 && i <= 39) { 
-          booked = "booked";
-      } else {
-          booked =  "";
-      }
-      
-      let seat1ID = 'S' + (i + 1); 
+    let booked = "";
+
+    if(k===3){
+      k=0;
+    }
+
+    if (i >= 0 && i <= 2) {
+      booked = "booked";
+
+      let seat1ID = 'S' + (i + 1);
       let grid1Id = i + 1;
+
+      const components = ["Serum", "Plasma", "Buffy coat"];
+      const componetId = components[k];
+      k++;
+
+      // Assign a unique ID to each label
+      let labelID = 'label_' + seat1ID;
+
       seats1.insertAdjacentHTML(
         "beforeend",
         '<input type="checkbox" name="tickets" id="' + seat1ID + '" />' +
-        '<label for="' + seat1ID + '" class="seat ' + booked + '"> BR_'+ grid1Id +'</label>'
+        '<label id="' + labelID + '" for="' + seat1ID + '" class="seat ' + booked + '"> BR_01' + " <br>" + componetId + '</label>'
+      );
+
+      // Set the font size for each seat's label
+      document.getElementById(labelID).style.fontSize = '10px';
+      document.getElementById(labelID).style.color = 'rgb(255, 255, 255)';
+      document.getElementById(labelID).style.textAlign = 'center';
+
+
+      seat1Array.push(seat1ID);
+
+      let seatCheckbox = document.getElementById(seat1ID);
+      seatCheckbox.addEventListener('click', function () {
+        openModal();
+      });
+    }
+    else if (i >= 3 && i <= 5) {
+      booked = "booked";
+
+      let seat1ID = 'S' + (i + 1);
+      let grid1Id = i + 1;
+
+      const components = ["Serum", "Plasma", "Buffy coat"];
+      const componetId = components[k];
+      k++;
+      // Assign a unique ID to each label
+      let labelID = 'label_' + seat1ID;
+
+      seats1.insertAdjacentHTML(
+        "beforeend",
+        '<input type="checkbox" name="tickets" id="' + seat1ID + '" />' +
+        '<label id="' + labelID + '" for="' + seat1ID + '" class="seat ' + booked + '"> BR_02' + " <br>" + componetId + '</label>'
+      );
+
+      // Set the font size for each seat's label
+      document.getElementById(labelID).style.fontSize = '10px';
+      document.getElementById(labelID).style.color = 'rgb(255, 255, 255)';
+      document.getElementById(labelID).style.textAlign = 'center';
+
+
+      seat1Array.push(seat1ID);
+
+      let seatCheckbox = document.getElementById(seat1ID);
+      seatCheckbox.addEventListener('click', function () {
+        openModal();
+      });
+    }
+    else if (i >= 6 && i <= 8) {
+      booked = "booked";
+
+      let seat1ID = 'S' + (i + 1);
+      let grid1Id = i + 1;
+
+      const components = ["Serum", "Plasma", "Buffy coat"];
+      const componetId = components[k];
+      k++;
+      // Assign a unique ID to each label
+      let labelID = 'label_' + seat1ID;
+
+      seats1.insertAdjacentHTML(
+        "beforeend",
+        '<input type="checkbox" name="tickets" id="' + seat1ID + '" />' +
+        '<label id="' + labelID + '" for="' + seat1ID + '" class="seat ' + booked + '"> BR_03' + " <br>" + componetId + '</label>'
+      );
+
+      // Set the font size for each seat's label
+      document.getElementById(labelID).style.fontSize = '10px';
+      document.getElementById(labelID).style.color = 'rgb(255, 255, 255)';
+      document.getElementById(labelID).style.textAlign = 'center';
+
+
+      seat1Array.push(seat1ID);
+
+      let seatCheckbox = document.getElementById(seat1ID);
+      seatCheckbox.addEventListener('click', function () {
+        openModal();
+      });
+    }
+    else if (i >= 9 && i <= 41) {
+      booked = "booked";
+
+      let seat1ID = 'S' + (i + 1);
+      let grid1Id = i + 1;
+
+      const components = ["Serum", "Plasma", "Buffy coat"];
+      const componetId = components[k];
+      k++;
+      // Assign a unique ID to each label
+      let labelID = 'label_' + seat1ID;
+
+      seats1.insertAdjacentHTML(
+        "beforeend",
+        '<input type="checkbox" name="tickets" id="' + seat1ID + '" />' +
+        '<label id="' + labelID + '" for="' + seat1ID + '" class="seat ' + booked + '"> BR_' + grid1Id + " <br>" + componetId + '</label>'
+      );
+
+      // Set the font size for each seat's label
+      document.getElementById(labelID).style.fontSize = '10px';
+      document.getElementById(labelID).style.color = 'rgb(255, 255, 255)';
+      document.getElementById(labelID).style.textAlign = 'center';
+
+
+      seat1Array.push(seat1ID);
+
+      let seatCheckbox = document.getElementById(seat1ID);
+      seatCheckbox.addEventListener('click', function () {
+        openModal();
+      });
+    }
+    else {
+      let seat1ID = 'S' + (i + 1);
+      seats1.insertAdjacentHTML(
+        "beforeend",
+        '<input type="checkbox" name="tickets" id="' + seat1ID + '" />' +
+        '<label for="' + seat1ID + '" class="seat ' + booked + '"> ' + '</label>'
+
       );
       seat1Array.push(seat1ID);
 
@@ -22,34 +149,58 @@ function test1() {
       seatCheckbox.addEventListener('click', function () {
         openModal();
       });
+    }
   }
-  console.log(seat1Array); 
+  console.log(seat1Array);
 }
 
 function test2() {
   let seats2 = document.querySelector(".specimen-box");
   let seat2Array = [];
   for (var j = 0; j < 54; j++) {
-      let booked = "";
-      if (j >= 0 && j <= 39) { 
-          booked = "booked";
-      } else {
-          booked = "";
-      }
-      
+    let booked = "";
+    if (j >= 0 && j <= 41) {
+      booked = "booked";
       let seat2ID = 'P' + (j + 1);
       let grid2Id = j + 1;
+
+      const components = ["FT","FN"];
+      const componetId = components[Math.floor(Math.random() * components.length)];
+      
+      let labelID = 'label_' + seat2ID;
+
       seats2.insertAdjacentHTML(
         "beforeend",
         '<input type="checkbox" name="tickets" id="' + seat2ID + '" />' +
-        '<label for="' + seat2ID + '" class="seat ' + booked + '"> SR_'+ grid2Id +'</label>'
+        '<label id="' + labelID + '"for="' + seat2ID + '" class="seat ' + booked + '"> BR_' + grid2Id + " <br>" + componetId +  '</label>'
       );
+
+      document.getElementById(labelID).style.fontSize = '10px';
+      document.getElementById(labelID).style.color = 'rgb(255, 255, 255)';
+      document.getElementById(labelID).style.textAlign = 'center';
+
       seat2Array.push(seat2ID);
 
       let seatCheckbox = document.getElementById(seat2ID);
       seatCheckbox.addEventListener('click', function () {
         openModal();
       });
+    } else {
+      let seat2ID = 'p' + (j + 1);
+      seats2.insertAdjacentHTML(
+        "beforeend",
+        '<input type="checkbox" name="tickets" id="' + seat2ID + '" />' +
+        '<label for="' + seat2ID + '" class="seat ' + booked + '"> ' + '</label>'
+
+      );
+      seat2Array.push(seat2ID);
+
+      let seatCheckbox = document.getElementById(seat2ID);
+      seatCheckbox.addEventListener('click', function () {
+        openModal();
+      });    }
+
+
   }
   console.log(seat2Array);
 }
